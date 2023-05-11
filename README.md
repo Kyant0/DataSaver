90517b8b277243c5682364c79ec2bbdd535fc7d9
Kyant0 <kyant2021@outlook.com>
2023-05-11 16:42:27 +0800

    Update compose to 1.5.0-alpha04

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ allprojects {
     }
 }
 
-implementation("com.github.Kyant0:DataSaver:2023.5.2")
+implementation("com.github.Kyant0:DataSaver:2023.5.3")
 ```
 
 ## Usage
--- datasaver/build.gradle.kts
+++ datasaver/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.kyant.datasaver"
-version = "2023.5.2"
+version = "2023.5.3"
 
 android {
     namespace = "com.kyant.datasaver"
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.runtime:runtime:1.5.0-alpha03")
+    implementation("androidx.compose.runtime:runtime:1.5.0-alpha04")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.5.0")
@@ -51,7 +51,7 @@ afterEvaluate {
             register("mavenRelease", MavenPublication::class) {
                 groupId = "com.kyant"
                 artifactId = "datasaver"
-                version = "2023.5.2"
+                version = "2023.5.3"
                 from(components["release"])
             }
         }

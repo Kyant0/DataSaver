df0b5d59f231f31b39d1980e3a7fcbcbef0485a8
Kyant0 <kyant2021@outlook.com>
2023-05-17 11:20:26 +0800

    Update dependencies

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ allprojects {
     }
 }
 
-implementation("com.github.Kyant0:DataSaver:2023.5.3")
+implementation("com.github.Kyant0:DataSaver:2023.5.5")
 ```
 
 ## Usage
--- datasaver/build.gradle.kts
+++ datasaver/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.kyant.datasaver"
-version = "2023.5.4"
+version = "2023.5.5"
 
 android {
     namespace = "com.kyant.datasaver"
@@ -41,8 +41,8 @@ android {
 dependencies {
     implementation("androidx.compose.runtime:runtime:1.5.0-alpha04")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.5.1")
 }
 
 afterEvaluate {
@@ -51,7 +51,7 @@ afterEvaluate {
             register("mavenRelease", MavenPublication::class) {
                 groupId = "com.kyant"
                 artifactId = "datasaver"
-                version = "2023.5.4"
+                version = "2023.5.5"
                 from(components["release"])
             }
         }

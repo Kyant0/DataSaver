4c13e93d20dde0329fd9d1bd616442635910ccad
Kyant0 <kyant2021@outlook.com>
2023-06-22 18:34:54 +0800

    Update compose to 1.6.0-alpha01

--- datasaver/build.gradle.kts
+++ datasaver/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.kyant.datasaver"
-version = "2023.6.1"
+version = "2023.6.2"
 
 android {
     namespace = "com.kyant.datasaver"
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.runtime:runtime:1.5.0-beta02")
+    implementation("androidx.compose.runtime:runtime:1.6.0-alpha01")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.5.1")
@@ -51,7 +51,7 @@ afterEvaluate {
             register("mavenRelease", MavenPublication::class) {
                 groupId = "com.kyant"
                 artifactId = "datasaver"
-                version = "2023.6.1"
+                version = "2023.6.2"
                 from(components["release"])
             }
         }

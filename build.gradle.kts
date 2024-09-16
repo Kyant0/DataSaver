7fd27583e60ca2d99fa114aef51e546df032cccb
Kyant <76829190+Kyant0@users.noreply.github.com>
2024-09-16 20:20:43 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
-    id("com.android.library") version "8.1.2" apply false
-    kotlin("android") version "1.9.20" apply false
-    kotlin("plugin.serialization") version "1.9.20" apply false
+    id("com.android.library") version "8.6.0" apply false
+    kotlin("android") version "2.0.20" apply false
+    kotlin("plugin.serialization") version "2.0.20" apply false
 }

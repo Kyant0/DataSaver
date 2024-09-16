e9a03622ef08149e0b585082627fcc8c89a7a48f
Kyant <76829190+Kyant0@users.noreply.github.com>
2024-09-16 20:26:16 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id("com.android.library") version "8.6.0" apply false
     kotlin("android") version "2.0.20" apply false
+    kotlin("plugin.compose") version "2.0.20" apply false
     kotlin("plugin.serialization") version "2.0.20" apply false
 }
